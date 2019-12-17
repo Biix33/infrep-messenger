9661e8005023f444051cad5fd3cd6a758bbee46d
Yoann <ybienvenu@mail.welcome-int.xyz>
2019-12-17 11:51:54 +0100

    adding alt prop on avatar img

--- client/src/components/Avatar/avatar.index.jsx
+++ client/src/components/Avatar/avatar.index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-export default function Avatar() {
-  return (
-    <div className="avatar">
-      <img src="https://i.pravatar.cc/24" />
-    </div>
-  );
+export default class Avatar extends React.Component {
+  render() {
+    return (
+      <div className="avatar">
+        <img src="https://i.pravatar.cc/24" alt={this.props.currentUser} />
+      </div>
+    );
+  }
 }
